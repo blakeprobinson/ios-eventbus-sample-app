25b74b495c5879ab77c981315bc90a9a8841d685
Robinson, Blake <Blake.Robinson@VerizonWireless.com>
2020-08-19 11:47:12 -0400

    Initial commit

---
+++ EventBusProject/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  EventBusProject
+//
+//  Created by Robinson, Blake on 8/19/20.
+//  Copyright © 2020 Verizon Wireless. All rights reserved.
+//
+
+import UIKit
+import EventBus
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+}
+
