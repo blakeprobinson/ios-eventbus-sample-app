08df81be0e09a33fdcc41fe590db718f80fb8bc5
Robinson, Blake <Blake.Robinson@VerizonWireless.com>
2020-08-20 17:55:24 -0400

    Project is ready to ship with the exception of warning fixes

---
+++ EventBusProject/EventOneDelegateSwitch.swift
@@ -0,0 +1,9 @@
+//
+//  EventOneDelegateSwitch.swift
+//  EventBusProject
+//
+//  Created by Robinson, Blake on 8/20/20.
+//  Copyright © 2020 Verizon Wireless. All rights reserved.
+//
+
+import Foundation
